b47751a166a74f39b7819c68c459ac32e1798bad
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-06 00:08:48 -0500

    commit tree a2f96e3685bf04717bb06067ca47f96c63ab5e96

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,37 +1,81 @@
-require 'pry'
-
 class String
 
-  def sentence?
-    if self[(self.length)-1] == "."
-      return true
-    else 
-      return false
+    def sentence?
+        if self.end_with?(".")
+          return true
+        else
+          return false
+        end
+      end
+
+      def question?
+        if self.end_with?("?")
+          return true
+        else
+          return false
+        end
+      end
+
+      def exclamation?
+        if self.end_with?("!")
+          return true
+        else
+          return false
+        end  
     end
-    
-  end
 
-  def question?
-    if self.end_with?("?") == true
-      return true
-    else
-      return false
+    def count_sentences
+      self.split(/[!?.]+/).count
+
     end
-  end
-
-
-  def exclamation?
-    if self.end_with?("!") == true
-      return true
-    else
-      return false
-    end
-
-  end
-
-  def count_sentences
-    self.split(/[!.?]+/).count
-
-  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+
+# class String
+
+#   def sentence?
+#     if self[(self.length)-1] == "."
+#       return true
+#     else 
+#       return false
+#     end
+    
+#   end
+
+#   def question?
+#     if self.end_with?("?") == true
+#       return true
+#     else
+#       return false
+#     end
+#   end
+
+
+#   def exclamation?
+#     if self.end_with?("!") == true
+#       return true
+#     else
+#       return false
+#     end
+
+#   end
+
+#   def count_sentences
+#     self.split(/[!.?]+/).count
+
+#   end
+
+# end
